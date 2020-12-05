e3b4c2fc799191473f83025439abb3caf58dd82f
renatazadravec <renatasilva2512@gmail.com>
2020-12-05 00:23:39 -0300

    Inner JOIN em produtos com categorias

--- Models/Produtos.php
+++ Models/Produtos.php
@@ -8,7 +8,7 @@ class Produtos
     {
         $connection = Connection::getDb();
 
-        $stmt = $connection->query("SELECT * FROM produtos");
+        $stmt = $connection->query("SELECT produtos.nome, produtos.descricao, produtos.preco, produtos.preco_promocao, produtos.imagem, categorias.nome categorianome, categorias.slug FROM `produtos` INNER JOIN `categorias` on produtos.categoria_id = categorias.id");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
